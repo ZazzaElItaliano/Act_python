b137219e2816b39fe96505bbf32b38be95c98655
tabs <adroidemesa@gmail.com>
2024-11-05 20:10:44 +0100

    ej 5-10 casi CABADOS

---
+++ bucles01/Ejercicios_6al10.py
@@ -0,0 +1,74 @@
+"""
+6.Sumar dígitos de un número con while
+Escribe un programa que sume los dígitos de un número utilizando un bucle while
+"""
+numero=int(input("Dime un numerito campeón: " ))
+suma=0
+
+while(numero>0):
+    suma+=numero%10
+    numero=numero//10
+    
+print(f"La suma de los dígitos es: {suma}")
+
+"""
+7.Adivinar el número con while
+Crea un juego en el que el usuario intente adivinar un número secreto. El programa deberá
+seguir solicitando al usuario que adivine hasta que lo haga correctamente
+"""
+n=77
+acertado=True
+while( acertado): 
+    preguntita = int(input(("Se ha generado un numerín secreto entre el 1 y el 100. Trata de averiguarlo mastodonte: ")))    
+    if(preguntita==n):
+            
+            print("Enhorabuena acertaste el numerín") 
+            acertado=False
+    else:
+        print("Has fallado, sigue intentándolo.")       
+        
+        
+"""
+8.Conversión de temperaturas con for
+Escribe un programa que convierta un rango de temperaturas de Celsius a Fahrenheit
+utilizando un bucle for.(ejercicio de listas)
+"""
+
+
+
+"""
+9.Serie de Fibonacci con for
+Escribe un programa que genere los primeros N números de la serie de Fibonacci utilizando
+un bucle for
+"""
+
+
+"""
+10. Conteo de vocales con for
+a. Escribe un programa que cuente cuántas vocales hay en una palabra dada utilizando un
+bucle for y una condición if
+"""
+print("Dime una palabra o frase y te contaré las vocales e incluso te dire cuantas de cada una hay")
+vocalizar=str(input("Frasecita/palabrita: "))
+vocalizar=vocalizar.lower()
+vocales=["a","e","i","o","u"]
+contador=0
+mapeador={
+    "a":0,
+    "e":0,
+    "i":0,
+    "o":0,
+    "u":0
+}
+for x in vocalizar:
+    if(x in vocales):
+        contador+=1
+print(f"Tu frase/palabra tiene: {contador} vocal/es")
+for y in vocalizar:
+    if(x in vocales):
+        contador+=1
+
+
+
+
+
