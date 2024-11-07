cf5dd19cab2eb198a525d4575be38c68e9549d75
tabs <adroidemesa@gmail.com>
2024-11-07 20:15:06 +0100

    ejercicios1-9

--- bucles02/Ejercicios_1al10.py
+++ bucles02/Ejercicios_1al10.py
@@ -41,5 +41,29 @@ for i in range(1, N + 1):
 print(f"La suma de los primeros {N} números es: {suma}")
 
 """
-
+6.Factorial de un número: Escribe un programa que calcule el factorial de un número
+usando un bucle
 """
+n = int(input("Ingresa un número para calcular su factorial: "))
+factorial = 1
+for i in range(1, n + 1):
+    factorial *= i
+print(f"El factorial de {n} es: {factorial}")
+
+"""7.Imprimir tabla de multiplicar: Solicita un número al usuario y genera su tabla de
+multiplicar del 1 al 10."""
+tabla=int(input("Dame el número dle cual quieres la tabla de multiplicar: "))
+for i in range(0,11):
+    print(f"{tabla}*{i}={tabla*i}")
+    
+"""
+9.Imprimir los primeros N números impares: Escribe un programa que imprima los
+primeros N números impares utilizando un bucle while
+"""
+N = int(input("Ingresa el valor de N: "))
+contador = 0  
+numero_impar = 1  
+while contador < N:
+    print(numero_impar)
+    numero_impar += 2  
+    contador += 1  
